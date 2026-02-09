e110e80c75da8bf13faa5b6c1c86c3d63bcb8e95
kaviya <kaviyarajasekaran247@gmail.com>
2026-02-09 23:26:13 +0530

    Update model_unetpp.py

--- model_unetpp.py
+++ model_unetpp.py
@@ -2,71 +2,68 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+
 class ConvBlock(nn.Module):
-    def __init__(self, in_ch, out_ch):
+    def __init__(self, in_c, out_c):
         super().__init__()
         self.conv = nn.Sequential(
-            nn.Conv2d(in_ch, out_ch, 3, padding=1),
-            nn.InstanceNorm2d(out_ch),
+            nn.Conv2d(in_c, out_c, 3, padding=1),
+            nn.BatchNorm2d(out_c),   # ← Use BatchNorm like Colab
             nn.ReLU(inplace=True),
-            nn.Conv2d(out_ch, out_ch, 3, padding=1),
-            nn.InstanceNorm2d(out_ch),
+            nn.Conv2d(out_c, out_c, 3, padding=1),
+            nn.BatchNorm2d(out_c),
             nn.ReLU(inplace=True)
         )
 
     def forward(self, x):
         return self.conv(x)
 
+
 class UNetPP(nn.Module):
     def __init__(self, in_ch=1, out_ch=1):
         super().__init__()
 
-        filters = [64, 128, 256, 512, 1024]
+        f = [32, 64, 128, 256]   # ← Match Colab
 
-        self.pool = nn.MaxPool2d(2, 2)
-        self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+        self.pool = nn.MaxPool2d(2)
 
-        self.conv0_0 = ConvBlock(in_ch, filters[0])
-        self.conv1_0 = ConvBlock(filters[0], filters[1])
-        self.conv2_0 = ConvBlock(filters[1], filters[2])
-        self.conv3_0 = ConvBlock(filters[2], filters[3])
-        self.conv4_0 = ConvBlock(filters[3], filters[4])
+        # Encoder
+        self.x0_0 = ConvBlock(in_ch, f[0])
+        self.x1_0 = ConvBlock(f[0], f[1])
+        self.x2_0 = ConvBlock(f[1], f[2])
+        self.x3_0 = ConvBlock(f[2], f[3])
 
-        self.conv0_1 = ConvBlock(filters[0]+filters[1], filters[0])
-        self.conv1_1 = ConvBlock(filters[1]+filters[2], filters[1])
-        self.conv2_1 = ConvBlock(filters[2]+filters[3], filters[2])
-        self.conv3_1 = ConvBlock(filters[3]+filters[4], filters[3])
+        # Nested Decoder
+        self.x0_1 = ConvBlock(f[0] + f[1], f[0])
+        self.x1_1 = ConvBlock(f[1] + f[2], f[1])
+        self.x2_1 = ConvBlock(f[2] + f[3], f[2])
 
-        self.conv0_2 = ConvBlock(filters[0]*2+filters[1], filters[0])
-        self.conv1_2 = ConvBlock(filters[1]*2+filters[2], filters[1])
-        self.conv2_2 = ConvBlock(filters[2]*2+filters[3], filters[2])
+        self.x0_2 = ConvBlock(f[0]*2 + f[1], f[0])
+        self.x1_2 = ConvBlock(f[1]*2 + f[2], f[1])
 
-        self.conv0_3 = ConvBlock(filters[0]*3+filters[1], filters[0])
-        self.conv1_3 = ConvBlock(filters[1]*3+filters[2], filters[1])
+        self.x0_3 = ConvBlock(f[0]*3 + f[1], f[0])
 
-        self.conv0_4 = ConvBlock(filters[0]*4+filters[1], filters[0])
-
-        self.final = nn.Conv2d(filters[0], out_ch, 1)
+        self.final = nn.Conv2d(f[0], out_ch, 1)
 
     def forward(self, x):
-        x0_0 = self.conv0_0(x)
-        x1_0 = self.conv1_0(self.pool(x0_0))
-        x2_0 = self.conv2_0(self.pool(x1_0))
-        x3_0 = self.conv3_0(self.pool(x2_0))
-        x4_0 = self.conv4_0(self.pool(x3_0))
+        # Encoder
+        x0_0 = self.x0_0(x)
+        x1_0 = self.x1_0(self.pool(x0_0))
+        x2_0 = self.x2_0(self.pool(x1_0))
+        x3_0 = self.x3_0(self.pool(x2_0))
 
-        x0_1 = self.conv0_1(torch.cat([x0_0, self.up(x1_0)], 1))
-        x1_1 = self.conv1_1(torch.cat([x1_0, self.up(x2_0)], 1))
-        x2_1 = self.conv2_1(torch.cat([x2_0, self.up(x3_0)], 1))
-        x3_1 = self.conv3_1(torch.cat([x3_0, self.up(x4_0)], 1))
+        # Decoder
+        x0_1 = self.x0_1(torch.cat([x0_0, F.interpolate(x1_0, scale_factor=2, mode="bilinear", align_corners=False)], 1))
+        x1_1 = self.x1_1(torch.cat([x1_0, F.interpolate(x2_0, scale_factor=2, mode="bilinear", align_corners=False)], 1))
+        x2_1 = self.x2_1(torch.cat([x2_0, F.interpolate(x3_0, scale_factor=2, mode="bilinear", align_corners=False)], 1))
 
-        x0_2 = self.conv0_2(torch.cat([x0_0, x0_1, self.up(x1_1)], 1))
-        x1_2 = self.conv1_2(torch.cat([x1_0, x1_1, self.up(x2_1)], 1))
-        x2_2 = self.conv2_2(torch.cat([x2_0, x2_1, self.up(x3_1)], 1))
+        x0_2 = self.x0_2(torch.cat([x0_0, x0_1,
+                                    F.interpolate(x1_1, scale_factor=2, mode="bilinear", align_corners=False)], 1))
 
-        x0_3 = self.conv0_3(torch.cat([x0_0, x0_1, x0_2, self.up(x1_2)], 1))
-        x1_3 = self.conv1_3(torch.cat([x1_0, x1_1, x1_2, self.up(x2_2)], 1))
+        x1_2 = self.x1_2(torch.cat([x1_0, x1_1,
+                                    F.interpolate(x2_1, scale_factor=2, mode="bilinear", align_corners=False)], 1))
 
-        x0_4 = self.conv0_4(torch.cat([x0_0, x0_1, x0_2, x0_3, self.up(x1_3)], 1))
+        x0_3 = self.x0_3(torch.cat([x0_0, x0_1, x0_2,
+                                    F.interpolate(x1_2, scale_factor=2, mode="bilinear", align_corners=False)], 1))
 
-        return torch.sigmoid(self.final(x0_4))
+        return torch.sigmoid(self.final(x0_3))
