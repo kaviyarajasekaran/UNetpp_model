b1abcdcddc0194392ba89cb09b9f269a0c5e89c8
kaviya <kaviyarajasekaran247@gmail.com>
2026-02-09 23:08:12 +0530

    Update model_unetpp.py

--- model_unetpp.py
+++ model_unetpp.py
@@ -7,10 +7,10 @@ class ConvBlock(nn.Module):
         super().__init__()
         self.conv = nn.Sequential(
             nn.Conv2d(in_ch, out_ch, 3, padding=1),
-            nn.BatchNorm2d(out_ch),
+            nn.InstanceNorm2d(out_ch),
             nn.ReLU(inplace=True),
             nn.Conv2d(out_ch, out_ch, 3, padding=1),
-            nn.BatchNorm2d(out_ch),
+            nn.InstanceNorm2d(out_ch),
             nn.ReLU(inplace=True)
         )
 
