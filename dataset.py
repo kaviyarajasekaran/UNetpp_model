cf05d60568cb7f9b6af9db71b0f85c8ffc6f3ef9
kaviya <kaviyarajasekaran247@gmail.com>
2026-02-09 14:49:24 +0530

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -5,19 +5,24 @@ from PIL import Image
 import albumentations as A
 from albumentations.pytorch import ToTensorV2
 
+
 class DrawingDataset(Dataset):
     def __init__(self, noisy_dir, clean_dir, split="train", val_ratio=0.1, img_size=384):
 
         self.noisy_dir = noisy_dir
         self.clean_dir = clean_dir
-
+        self.split = split
         self.files = sorted(os.listdir(noisy_dir))
         print("Total images:", len(self.files))
+
+        split_idx = int(len(self.files) * (1 - val_ratio))
 
         if split == "train":
             self.files = self.files[:split_idx]
         else:
             self.files = self.files[split_idx:]
+
+        print(f"{split} size:", len(self.files))
 
         if split == "train":
             self.transform = A.Compose([
@@ -38,13 +43,13 @@ class DrawingDataset(Dataset):
 
     def __len__(self):
         return len(self.files)
-
     def __getitem__(self, idx):
-        noisy = Image.open(os.path.join(self.noisy_dir, self.files[idx])).convert("L")
-        clean = Image.open(os.path.join(self.clean_dir, self.files[idx])).convert("L")
+        noisy_path = os.path.join(self.noisy_dir, self.files[idx])
+        clean_path = os.path.join(self.clean_dir, self.files[idx])
 
-        noisy = np.array(noisy)
-        clean = np.array(clean)
+        noisy = np.array(Image.open(noisy_path).convert("L"), dtype=np.float32)
+        clean = np.array(Image.open(clean_path).convert("L"), dtype=np.float32)
 
         aug = self.transform(image=noisy, mask=clean)
+
         return aug["image"], aug["mask"]
