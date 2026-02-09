5eab0b2e08a6d1c4eea394ef476fb94589ff26e5
kaviya <kaviyarajasekaran247@gmail.com>
2026-02-09 15:17:15 +0530

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -2,28 +2,34 @@ import os
 import numpy as np
 from torch.utils.data import Dataset
 from PIL import Image
+from sklearn.model_selection import train_test_split
 import albumentations as A
 from albumentations.pytorch import ToTensorV2
 
-class DrawingDataset(Dataset):
+class DenoisingDataset(Dataset):
     def __init__(self, noisy_dir, clean_dir, split="train", val_ratio=0.2, img_size=384):
         self.noisy_dir = noisy_dir
         self.clean_dir = clean_dir
-        self.split = split
+        self.split = split.lower()
 
-        self.files = sorted(os.listdir(noisy_dir))
-        print("Total images:", len(self.files))
+        noisy_files = set(os.listdir(noisy_dir))
+        clean_files = set(os.listdir(clean_dir))
+        self.files = sorted(list(noisy_files.intersection(clean_files)))
 
-        split_idx = int(len(self.files) * (1 - val_ratio))
-        self.files = self.files[:split_idx] if split=="train" else self.files[split_idx:]
-        print(f"{split} size:", len(self.files))
+        print(f"Total matched images: {len(self.files)}")
 
-        if split == "train":
+        train_files, val_files = train_test_split(
+            self.files, test_size=val_ratio, random_state=42
+        )
+        self.files = train_files if self.split == "train" else val_files
+        print(f"{split} size: {len(self.files)}")
+
+        if self.split == "train":
             self.transform = A.Compose([
                 A.Resize(img_size, img_size),
                 A.HorizontalFlip(p=0.5),
-                A.VerticalFlip(p=0.5),
-                A.RandomRotate90(p=0.5),
+                A.VerticalFlip(p=0.3),
+                A.RandomRotate90(p=0.3),
                 A.GaussNoise(p=0.2),
                 ToTensorV2()
             ])
@@ -37,8 +43,16 @@ class DrawingDataset(Dataset):
         return len(self.files)
 
     def __getitem__(self, idx):
-        noisy = np.array(Image.open(os.path.join(self.noisy_dir, self.files[idx])).convert("L")) / 255.0
-        clean = np.array(Image.open(os.path.join(self.clean_dir, self.files[idx])).convert("L")) / 255.0
+        fname = self.files[idx]
+
+        noisy_path = os.path.join(self.noisy_dir, fname)
+        clean_path = os.path.join(self.clean_dir, fname)
+
+        noisy = np.array(Image.open(noisy_path).convert("L"), dtype=np.float32) / 255.0
+        clean = np.array(Image.open(clean_path).convert("L"), dtype=np.float32) / 255.0
 
         aug = self.transform(image=noisy, mask=clean)
-        return aug["image"].float(), aug["mask"].float()
+        noisy = aug["image"].float()
+        clean = aug["mask"].float()
+
+        return noisy, clean
