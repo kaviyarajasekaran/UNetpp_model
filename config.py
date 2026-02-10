94869cee0f1c11c225bb81390b904f964411bc7c
kaviya <kaviyarajasekaran247@gmail.com>
2026-02-10 20:01:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 class Config:
     noisy_dir = "/kaggle/input/input-data2k/2kdata/Dataset-1k/New_Data100/Noisy"
     clean_dir = "/kaggle/input/input-data2k/2kdata/Dataset-1k/New_Data100/Clean"
-    img_size = 512
+    img_size = 384
     channels = 1
     batch_size = 2
     epochs = 30
-    lr = 1e-4
+    lr = 5e-5
     checkpoint_dir = "checkpoints"
     prediction_dir = "predictions"
     val_ratio = 0.2
